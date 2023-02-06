066e950cebf3128a717f1383276a0b9f0977a5eb
Xero kun <elo48858@gmail.com>
2023-02-07 00:01:42 +0100

    `⚡UPDATE` Added 14 More Pokemons

--- app/src/main/java/dev/xero/pokemon/data/PokemonData.kt
+++ app/src/main/java/dev/xero/pokemon/data/PokemonData.kt
@@ -193,11 +193,179 @@ class PokemonData {
 			imageResID = R.drawable.pidgey,
 			nameResID = R.string.pidgey,
 			descriptionResID = R.string.pidgey_description,
+			healthPower = 15,
+			attackPower = 15,
+			defensePower = 15,
+			type = listOf("Normal"),
+			weaknesses = listOf("Electric", "Ice", "Rock")
+		),
+
+		Pokemon(
+			number = 17,
+			imageResID = R.drawable.pidgeotto,
+			nameResID = R.string.pidgeotto,
+			descriptionResID = R.string.pidgeotto_description,
 			healthPower = 20,
 			attackPower = 20,
 			defensePower = 20,
 			type = listOf("Normal"),
 			weaknesses = listOf("Electric", "Ice", "Rock")
+		),
+
+		Pokemon(
+			number = 18,
+			imageResID = R.drawable.pidgeot,
+			nameResID = R.string.pidgeot,
+			descriptionResID = R.string.pidgeot_description,
+			healthPower = 30,
+			attackPower = 30,
+			defensePower = 30,
+			type = listOf("Normal"),
+			weaknesses = listOf("Electric", "Ice", "Rock")
+		),
+
+		Pokemon(
+			number = 19,
+			imageResID = R.drawable.rattata,
+			nameResID = R.string.rattata,
+			descriptionResID = R.string.rattata_description,
+			healthPower = 10,
+			attackPower = 20,
+			defensePower = 15,
+			type = listOf("Normal"),
+			weaknesses = listOf("Fighting")
+		),
+
+		Pokemon(
+			number = 20,
+			imageResID = R.drawable.raticate,
+			nameResID = R.string.raticate,
+			descriptionResID = R.string.raticate_description,
+			healthPower = 20,
+			attackPower = 25,
+			defensePower = 20,
+			type = listOf("Normal"),
+			weaknesses = listOf("Fighting")
+		),
+
+		Pokemon(
+			number = 21,
+			imageResID = R.drawable.spearow,
+			nameResID = R.string.spearow,
+			descriptionResID = R.string.spearow_description,
+			healthPower = 15,
+			attackPower = 20,
+			defensePower = 10,
+			type = listOf("Normal"),
+			weaknesses = listOf("Electric", "Ice", "Rock")
+		),
+
+		Pokemon(
+			number = 22,
+			imageResID = R.drawable.fearow,
+			nameResID = R.string.fearow,
+			descriptionResID = R.string.fearow_description,
+			healthPower = 20,
+			attackPower = 40,
+			defensePower = 25,
+			type = listOf("Normal"),
+			weaknesses = listOf("Electric", "Ice", "Rock")
+		),
+
+		Pokemon(
+			number = 23,
+			imageResID = R.drawable.ekans,
+			nameResID = R.string.ekans,
+			descriptionResID = R.string.ekans_description,
+			healthPower = 15,
+			attackPower = 20,
+			defensePower = 15,
+			type = listOf("Poison"),
+			weaknesses = listOf("Psychic")
+		),
+
+		Pokemon(
+			number = 24,
+			imageResID = R.drawable.arbok,
+			nameResID = R.string.arbok,
+			descriptionResID = R.string.arbok_description,
+			healthPower = 25,
+			attackPower = 40,
+			defensePower = 30,
+			type = listOf("Poison"),
+			weaknesses = listOf("Psychic")
+		),
+
+		Pokemon(
+			number = 25,
+			imageResID = R.drawable.pikachu,
+			nameResID = R.string.pikachu,
+			descriptionResID = R.string.pikachu_description,
+			healthPower = 15,
+			attackPower = 20,
+			defensePower = 15,
+			type = listOf("Electric"),
+			weaknesses = listOf("Ground")
+		),
+
+		Pokemon(
+			number = 26,
+			imageResID = R.drawable.raichu,
+			nameResID = R.string.raichu,
+			descriptionResID = R.string.raichu_description,
+			healthPower = 20,
+			attackPower = 40,
+			defensePower = 20,
+			type = listOf("Electric"),
+			weaknesses = listOf("Ground")
+		),
+
+		Pokemon(
+			number = 27,
+			imageResID = R.drawable.sandshrew,
+			nameResID = R.string.sandshrew,
+			descriptionResID = R.string.sandshrew_description,
+			healthPower = 15,
+			attackPower = 25,
+			defensePower = 25,
+			type = listOf("Ground"),
+			weaknesses = listOf("Water", "Grass", "Ice")
+		),
+
+		Pokemon(
+			number = 28,
+			imageResID = R.drawable.sandslash,
+			nameResID = R.string.sandslash,
+			descriptionResID = R.string.sandslash_description,
+			healthPower = 25,
+			attackPower = 40,
+			defensePower = 45,
+			type = listOf("Ground"),
+			weaknesses = listOf("Water", "Grass", "Ice")
+		),
+
+		Pokemon(
+			number = 29,
+			imageResID = R.drawable.nidoran,
+			nameResID = R.string.nidoran,
+			descriptionResID = R.string.nidoran_description,
+			healthPower = 20,
+			attackPower = 15,
+			defensePower = 20,
+			type = listOf("Poison"),
+			weaknesses = listOf("Psychic", "Ground")
+		),
+
+		Pokemon(
+			number = 30,
+			imageResID = R.drawable.nidorina,
+			nameResID = R.string.nidorina,
+			descriptionResID = R.string.nidorina_description,
+			healthPower = 25,
+			attackPower = 20,
+			defensePower = 20,
+			type = listOf("Poison"),
+			weaknesses = listOf("Psychic", "Ground")
 		),
 
 	)
--- app/src/main/java/dev/xero/pokemon/models/MapOfTypeToColor.kt
+++ app/src/main/java/dev/xero/pokemon/models/MapOfTypeToColor.kt
@@ -11,9 +11,11 @@ class MapOfTypeToColor {
 		"Fire" to Color(0xFFEF526E),
 		"Electric" to Color(0xFFF7C545),
 		"Normal" to Color(0xFF89A4B6),
+		"Fighting" to Color(0xFFFF9437),
 		"Ice" to Color(0xFF5685E0),
 		"Water" to Color(0xFF465BE6),
 		"Rock" to Color(0xFF864E3D),
+		"Ground" to Color(0xFFBEAB64),
 		"Bug" to Color(0xFF51CA37),
 		"Poison" to Color(0xFFBC56E0),
 		"Psychic" to Color(0xFFF153BC)
